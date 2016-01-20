b724d90fc38cbbf0a80aabdf584ff619f9698509
beta-ray70 <barbier.julien@orange.fr>
2016-01-20 10:50:35 +0100

    End of the written of Map.cpp. Waiting for addition.

--- src/Map.cpp
+++ src/Map.cpp
@@ -9,6 +9,18 @@
 #include <Coord>
 #include <SFML/Graphics>
 
+#define RESOLUTION_X_IMAGE 32
+#define RESOLUTION_Y_IMAGE 32
+
+/*Map::Map : function to create with parametre the map for the game.
+ * All the parametre must be different of undefined.
+ * x : the lenght of the map.
+ * y : the width of the map.
+ * texture : array of texture wich contain all the texture needed to print the map.
+ * egg_sprite : texture of the egg.
+ * return the map created.
+ */
+
 Map::Map(unsigned char x, unsigned char y, Area smap[x][y], sf::Texture texture[NB_TEXTURE], sf::Texture egg_texture){
     
     x_size = x;
@@ -29,8 +41,10 @@ Map::Map(unsigned char x, unsigned char y, Area smap[x][y], sf::Texture texture[
     egg_sprite = egg_texture;
 }
 
+/*printAll : procedure to print all the map.
+ */
 
-void printAll (){
+void Map::printAll (){
     
     Coord tile;
     
@@ -41,5 +55,29 @@ void printAll (){
     }
 }
 
-	    
+/*print : print a tile of the map.
+ * tile : coordinate of the tile we want to print.
+ */
+
+void Map::print (Coord tile){
+    
+    map[tile.x][tile.y].setPosition(tile.x*RESOLUTION_X_IMAGE, tile.y*RESOLUTION_Y_IMAGE);
+    windows.draw(map[tile.x][tile.y]);
+    
+}
+
+/*popEgg : change the coordinate of the egg and print it.
+ * egg_coord : new coordinate of the egg.
+ */
+
+void Map::popEgg (Coord egg_coord){
+    
+    coordinate_egg = egg_coord;
+    
+    egg_sprite.setPosition(coordinate_egg.x*RESOLUTION_X_IMAGE, coordinate_egg.y*RESOLUTION_Y_IMAGE);
+    windows.draw(egg_sprite);
+    
+}
+
+
 	    
