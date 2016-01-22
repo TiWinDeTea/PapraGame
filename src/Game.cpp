f37d0727ed0d2be0cc7e392dc01ffd6c4987a1a6
pinam45 <maxime.pin@live.fr>
2016-01-22 16:01:09 +0100

    Ducks with controls !!!

--- src/Game.cpp
+++ src/Game.cpp
@@ -132,6 +132,7 @@ bool Game::loadMap(){
 
 void Game::start()
 {
+	unsigned int tmp(0);
 	std::vector< std::vector<sf::Keyboard::Key> > player_keys;
 	player_keys.push_back(std::vector<sf::Keyboard::Key>());
 	player_keys[0].push_back(sf::Keyboard::Up);
@@ -150,34 +151,40 @@ void Game::start()
 	Coord egg_coo;
 	egg_coo.x = 5;
 	egg_coo.y = 7;
+	sf::Event event;
 	game_map.popEgg(egg_coo, game_window);
 
-	sf::Event event;
+	game_window.display();
 	while (game_window.isOpen())
 	{
 		game_window.clear();
+		game_map.print(game_window);
 		while (game_window.pollEvent(event))
 		{
 			if(event.type == sf::Event::Closed)
 				game_window.close();
 			else if(event.type == sf::Event::KeyPressed){
 				for(unsigned char i = PLAYER_NUMBER; i--;){
-					if(sf::Keyboard::isKeyPressed(player_keys[i][1]))
+					if(sf::Keyboard::isKeyPressed(player_keys[i][0]))
 					    player_dir[i] = UP;
-					else if(sf::Keyboard::isKeyPressed(player_keys[i][2]))
+					else if(sf::Keyboard::isKeyPressed(player_keys[i][1]))
 					    player_dir[i] = DOWN;
-					else if(sf::Keyboard::isKeyPressed(player_keys[i][3]))
+					else if(sf::Keyboard::isKeyPressed(player_keys[i][2]))
 					    player_dir[i] = LEFT;
-					else if(sf::Keyboard::isKeyPressed(player_keys[i][4]))
+					else if(sf::Keyboard::isKeyPressed(player_keys[i][3]))
 					    player_dir[i] = RIGHT;
 				}
 			}
 		}
-		for(unsigned char i = PLAYER_NUMBER; i--;)
-			player[i].move(game_window, player_dir[i]);
+		sf::sleep(sf::milliseconds(50));
+		++tmp;
 
-		sf::sleep(sf::milliseconds(100));
-		game_map.print(game_window);
-		game_window.display();
+		if(tmp > 20){
+			tmp = 0;
+			for(unsigned char i = PLAYER_NUMBER; i--;)
+				player[i].move(game_window, player_dir[i]);
+
+			game_window.display();
+		}
 	}
 }
