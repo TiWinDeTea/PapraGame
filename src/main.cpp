9087ae7599d9b388e2fb4dcec5100013ca9f479d
Lucas Lazare <lazarelucas@yahoo.fr>
2016-01-26 16:06:03 +0100

    adding --version argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Game.hpp>
+#include <string>
 
 int main(int argc, char* argv[]);
 int main(int argc, char* argv[]){
@@ -7,8 +8,28 @@ int main(int argc, char* argv[]){
 		game.launch();
 	}
 	else{
-		Game game("res/", std::string(argv[1]) + '/', "ducks/");
-		game.launch();
+		if (std::string(argv[1]) == "--version") {
+			std::cout << "PapraGame v0.3" << std::endl <<
+				"Copyright c 2016 TiWinDeTea (https://github.com/TiWinDeTea)" << std::endl <<
+				std::endl <<
+				"Covered Software is provided under this License on an \"as is\"" << std::endl <<
+				"basis, without warranty of any kind, either expressed, implied, or" << std::endl <<
+				"statutory, including, without limitation, warranties that the" << std::endl <<
+				"Covered Software is free of defects, merchantable, fit for a" << std::endl <<
+				"particular purpose or non-infringing. The entire risk as to the" << std::endl <<
+				"quality and performance of the Covered Software is with You." << std::endl <<
+				"Should any Covered Software prove defective in any respect, You" << std::endl <<
+				"(not any Contributor) assume the cost of any necessary servicing," << std::endl <<
+				"repair, or correction. This disclaimer of warranty constitutes an" << std::endl <<
+				"essential part of this License. No use of any Covered Software is" << std::endl <<
+				"authorized under this License except under this disclaimer." << std::endl <<
+				std::endl <<
+				"Game currently under development" << std::endl;
+		}
+		else{
+			Game game("res/", std::string(argv[1]) + '/', "ducks/");
+			game.launch();
+		}
 	}
 	return 0;
 }
